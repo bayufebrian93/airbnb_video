249c5560dd01c4277682ed60f8e1a269015ef016
Bayu Febrian <out1273@fifgroup.co.id>
2023-04-07 23:14:13 +0700

    Register Modal - Component modal

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -4,9 +4,11 @@ import { AiOutlineMenu } from 'react-icons/ai';
 import Avatar from '../Avatar';
 import { useCallback, useState } from 'react';
 import MenuItem from './MenuItem';
+import useRegisterModal from '@/app/hooks/useRegisterModal';
 
 
 const UserMemnu = () => {
+  const registerModal = useRegisterModal();
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleOpen = useCallback(() => {
@@ -81,7 +83,7 @@ const UserMemnu = () => {
                 label='Login'
               />
               <MenuItem 
-                onClick={() => {}}
+                onClick={registerModal.onOpen}
                 label='Sign up'
               />
             </>
